718be1d7ea327a6a208dc0b8614510700aec0968
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-04-29 00:36:21 +0900

    :sparkles: feat: 학교 정보를 위한 모델 추가

---
+++ src/main/java/com/foreignlove/school/model/School.java
@@ -0,0 +1,48 @@
+package com.foreignlove.school.model;
+
+import com.foreignlove.nation.model.Nation;
+import lombok.AllArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class School {
+    private UUID id;
+    private Nation nation;
+    private String name;
+
+    public School(Nation nation, String name) {
+        this.nation = nation;
+        this.name = name;
+    }
+
+    public Map<String, Object> getParamMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id.toString());
+        map.put("name", name);
+        map.put("nation", nation.toString());
+        return map;
+    }
+
+    public Response getResponse() {
+        return new Response(id, name, nation.getResponse());
+    }
+
+    public record Request(
+        UUID id,
+        String name
+    ) {}
+
+    public record Response(
+        UUID id,
+        String name,
+        Nation.Response nation
+    ) {}
+
+    @Override
+    public String toString() {
+        return id.toString();
+    }
+}
