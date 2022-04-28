3a0995f0b2ee04963373384d021b6704781e644a
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-04-29 00:27:22 +0900

    :tada: chore: 프로젝트 설정 추가

---
+++ src/main/java/com/foreignlove/ForeignLoveApplication.java
@@ -0,0 +1,13 @@
+package com.foreignlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeignLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foreignlove/ForeignLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foreignlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
