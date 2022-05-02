db05259728c0b5c7f72f73b25e8b75987483b4d3
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-05-02 23:12:47 +0900

    :sparkles: feat: 자유게시판 모델 추가

---
+++ src/main/java/com/foreignlove/board/free/model/FreeBoard.java
@@ -0,0 +1,57 @@
+package com.foreignlove.board.free.model;
+
+import com.fasterxml.uuid.Generators;
+import com.foreignlove.board.model.Board;
+import com.foreignlove.user.model.User;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+public class FreeBoard extends Board {
+    public FreeBoard(String title, String content, User user) {
+        super(Generators.timeBasedGenerator().generate(), user, LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS), null);
+        this.title = title;
+        this.content = content;
+        this.imageUrl = null;
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+    }
+
+    public FreeBoard(UUID id, String title, String content, User user, String imageUrl, LocalDateTime createdAt,
+                     LocalDateTime updatedAt, LocalDateTime deletedAt) {
+        super(id, user, createdAt, deletedAt);
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.updatedAt = updatedAt;
+    }
+
+    public Map<String, Object> getParamMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id.toString());
+        map.put("title", title);
+        map.put("content", content);
+        map.put("userId", user.getId().toString());
+        map.put("imageUrl", imageUrl);
+        map.put("createdAt", createdAt);
+        map.put("updatedAt", updatedAt);
+        map.put("deletedAt", deletedAt);
+        return map;
+    }
+
+    public Response getResponse() {
+        return new Response(id, title, content, user.getResponse(), imageUrl, createdAt);
+    }
+
+    public record Response(
+        UUID id,
+        String title,
+        String content,
+        User.Response user,
+        String imageUrl,
+        LocalDateTime createdAt
+    ) {}
+}
